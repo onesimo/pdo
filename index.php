355394a1927880c72851f7f247ce2f39cd1e14c7
Onesimo Batista <onesimobatista@gmail.com>
2016-09-28 00:03:33 -0300

    new files

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php 
-
-echo "teste"; 
+try{
+	$conn = new \PDO('mysql:host=localhost;dbname=test_oo','root','');
+}catch(\PDOException $e){
+	echo "Error: Message: ".getMessage()." Code:".$e->getCode();
+}
